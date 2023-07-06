bf1d537328e27f2effb271604c937f2cc0d9db39
choewy <choewy32@gmail.com>
2023-07-06 22:33:36 +0900

    feature/use routers

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 
+import { RouterImpl } from './routers';
+
 export const createApp = (): Express => {
   const app = express();
 
   app.use(cors());
 
   return app;
+};
+
+export const useRouters = (app: Express, ...Routers: RouterImpl[]): void => {
+  Routers.forEach((Router) => {
+    const router = Router();
+
+    app.use(router.prefix, router.router);
+  });
 };
