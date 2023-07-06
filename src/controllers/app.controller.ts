dce68c4091ca8ee3d1631a56a28c5d117be6638d
choewy <choewy32@gmail.com>
2023-07-07 00:13:13 +0900

    feature/comments

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -5,10 +5,12 @@ import { AppService } from '../services';
 export const AppController = () => {
   const appService = AppService();
 
+  /** @test curl -X GET http://localhost:4000 */
   const sayHi: Handler = (_, res, __) => {
     return res.send({ message: appService.getMessage() });
   };
 
+  /** @test curl -X GET http://localhost:4000/versions */
   const getVersion: Handler = (_, res, __) => {
     return res.send({ version: appService.getVersion() });
   };
