73179165f1a59ec86057153d7e9b93cf8f1693eb
choewy <choewy32@gmail.com>
2023-07-06 23:28:16 +0900

    feature/temp database and interfaces

---
+++ src/repositories/database.ts
@@ -0,0 +1,8 @@
+import { Database } from './interfaces';
+
+export const database: Database = {
+  users: {
+    index: 0,
+    rows: [],
+  },
+};
---
+++ src/repositories/interfaces.ts
@@ -0,0 +1,16 @@
+import { UserEntity } from '../entities';
+
+export interface Database {
+  users: {
+    index: number;
+    rows: UserEntity[];
+  };
+}
+
+export interface BaseRepositoryImpl<T> {
+  find(conditions?: Partial<T>): Promise<T[]>;
+  findOne(conditions?: Partial<T>): Promise<T | null>;
+  insert(entity: T): Promise<boolean>;
+  update(conditions: Partial<T>, entity: T): Promise<boolean>;
+  save(entity: T): Promise<T>;
+}
