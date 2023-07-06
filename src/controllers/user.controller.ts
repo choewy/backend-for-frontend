c0bbc59f18720f94faa1eb7a95173d55ddfa6a0f
choewy <choewy32@gmail.com>
2023-07-06 23:46:30 +0900

    feature/user controller - get user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,15 +1,33 @@
 import { Handler } from 'express';
 
+import { HttpError, NotFoundError } from '../core';
 import { UserService } from '../services';
+
+import { GetUserParamsDto } from './user.dtos';
 
 export const UserController = () => {
   const userService = UserService();
 
   const getUsers: Handler = async (_, res, __) => {
-    return res.send({ rows: await userService.getUsers() });
+    return res.send(await userService.getUsers());
   };
 
-  const getUser: Handler = (req, res, next) => {};
+  const getUser: Handler = async (req, res, __) => {
+    try {
+      const body = GetUserParamsDto(req.params);
+      const user = await userService.getUserById(body.id);
+
+      if (!user) {
+        throw NotFoundError('not found user');
+      }
+
+      return res.send(user);
+    } catch (e) {
+      const error = e as HttpError;
+
+      res.status(error.status).send(error);
+    }
+  };
 
   const createUser: Handler = (req, res, next) => {};
   const updateUser: Handler = (req, res, next) => {};
